404a97ca6f2d1955b9cc4139d019e22438c8add2
Christopher Valerio <cvalerio@wearegap.com>
2015-05-03 22:17:06 -0600

    adding some fixes for getting mongo run

--- recipes/mongo_gem.rb
+++ recipes/mongo_gem.rb
@@ -17,7 +17,16 @@ package sasldev_pkg do
 end.run_action(:install)
 
 node['mongodb']['ruby_gems'].each do |gem, version|
-  chef_gem gem do
-    version version
+  if(Gem.const_defined?("Version") and Gem::Version.new(Chef::VERSION) < Gem::Version.new('10.12.0'))
+    gem_package 'mongo' do
+      version '1.12.1'
+      action :nothing
+    end.run_action(:install)
+    Gem.clear_paths
+  else
+    chef_gem 'mongo' do
+      version '1.12.1'
+      action :install
+    end
   end
 end
